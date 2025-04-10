095bceec7958b41197a6938b95f0f348f24b6742
elianaakim <140842030+elianaakim@users.noreply.github.com>
2025-04-09 22:09:15 -0700

    Added background picture and a text

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -9,22 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello my kwk members!")
-            .font(.title)
-            .fontWeight(.bold)
-        Text("My name is Eliana.")
-            .font(.title)
-            .fontWeight(.bold)
-        VStack(alignment: .leading, spacing: 20.0) {
-            Image("profile")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(20)
+        ZStack {
+            Color(.systemYellow)
+                .ignoresSafeArea()
+
+            VStack(alignment: .leading, spacing: 20.0) {
+                Text("Hello my kwk members!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("My name is Eliana.")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("Nice to meet you!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                    .frame(width: 300, height: 300)
+            }
+            .padding()
         }
-        .padding()
-        .frame(width: 300, height: 300)
-        
-    }
+    }}
 }
 
 #Preview {
